4514140f46e6068618f565ebf4731d7d4bb11e1d
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-12-02 11:00:46 +0330

    Added the Input component

---
+++ apps/web/components/Input/index.tsx
@@ -0,0 +1,27 @@
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  disabled?: boolean;
+  value?: string;
+  label?: string;
+  onChange(value: string): void;
+};
+export default function Input({
+  disabled = false,
+  value,
+  onChange,
+  label,
+}: Props) {
+  return (
+    <div className="w-full h-max flex flex-col items-start justify-start gap-2">
+      <span className="font-semibold text-black text-xl">{label}</span>
+      <input
+        type="{{$this->type}}"
+        value={value}
+        disabled={disabled}
+        className="w-full h-10 text-black outline-none bg-white flex items-center justify-start px-3 rounded-xl"
+        onChange={({ currentTarget: { value } }) => onChange(value)}
+      />
+    </div>
+  );
+}
