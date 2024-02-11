5ae0c27fe11cad52dfa8f60b8935a038ca4d7870
wexsilon <wexsilon2001@protonmail.com>
2024-02-11 13:31:21 +0330

    Comment code

--- apps/backend/src/modules/cart/dtos/update-cart.dto.ts
+++ apps/backend/src/modules/cart/dtos/update-cart.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateCartDto } from './create-cart.dto';
+// import { CreateCartDto } from './add-to-cart.dto';
 
-export class UpdateCartDto extends PartialType(CreateCartDto) {}
+// export class UpdateCartDto extends PartialType(CreateCartDto) {}
