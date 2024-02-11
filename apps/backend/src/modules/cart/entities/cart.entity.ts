3ddc7f86f754ba55b47226e6ff619613987b85f4
wexsilon <wexsilon2001@protonmail.com>
2024-02-11 13:28:22 +0330

    Create cart entity

--- apps/backend/src/modules/cart/entities/cart.entity.ts
+++ apps/backend/src/modules/cart/entities/cart.entity.ts
@@ -1,5 +1,28 @@
-import BaseModel from "src/common/entities/base-model.entity";
+import {
+  Collection,
+  Entity,
+  OneToMany,
+  OneToOne,
+  Property,
+} from '@mikro-orm/core';
+import BaseModel from 'src/common/entities/base-model.entity';
+import Product from 'src/modules/product/entities/product.entity';
+import User from 'src/modules/user/entities/user.entity';
 
+@Entity({ tableName: 'carts' })
 export default class Cart extends BaseModel {
-    
+  @Property({ type: 'double', default: 0 })
+  totalPrice: number | string;
+
+  @Property({ name: 'stripe_id', nullable: true })
+  stripeId?: string;
+
+  @OneToMany(() => Product, (product) => product.cart)
+  products = new Collection<Product>(this);
+
+  @OneToOne(() => User, (user) => user.cart, {
+    orphanRemoval: true,
+    nullable: true,
+  })
+  user?: User;
 }
