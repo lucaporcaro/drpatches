eef7e2539ff7e87beb16c399a4dffa5169d445ec
wexsilon <wexsilon2001@protonmail.com>
2024-02-11 13:32:19 +0330

    Register the cart entity

--- apps/backend/src/modules/cart/cart.module.ts
+++ apps/backend/src/modules/cart/cart.module.ts
@@ -3,9 +3,11 @@ import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
 import Cart from './entities/cart.entity';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import User from '../user/entities/user.entity';
+import Product from '../product/entities/product.entity';
 
 @Module({
-  //imports: [MikroOrmModule.forFeature([Cart])],
+  imports: [MikroOrmModule.forFeature({ entities: [Cart, User, Product] })],
   controllers: [CartController],
   providers: [CartService],
 })
