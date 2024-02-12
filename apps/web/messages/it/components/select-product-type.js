5142a662e8d9e175e96046d3bbbda7762f1e298f
PUYA\puya2 <puya.bahrani@eprojectsrl.it>
2024-02-12 14:35:48 +0100

    fix translation patch personalizzata

--- apps/web/messages/it/components/select-product-type.js
+++ apps/web/messages/it/components/select-product-type.js
@@ -1,17 +1,17 @@
 export default {
-  title: "Come desideri creare il tuo patch?",
+  title: "Come desideri creare la tua patch?",
   description:
     "Puoi scegliere tra i due metodi che offriamo ai clienti per creare un patch personalizzato:",
   types: {
     image: {
       title: "Immagine",
       description:
-        "Puoi creare il tuo patch personalizzato con l'immagine che desideri.",
+        "Puoi creare la tua patch personalizzata con l'immagine che desideri.",
     },
     text: {
       title: "Testo",
       description:
-        "Puoi creare il tuo patch personalizzato con il testo che desideri.",
+        "Puoi creare la tua patch personalizzata con il testo che desideri.",
     },
   },
 };
