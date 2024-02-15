40d1b716a29b3b9c893b760c438c7f00e78280d6
mhscan <mr.rezaeeian@gmail.com>
2024-02-15 16:47:42 +0330

    seve in local storeg

--- apps/web/app/[locale]/product/checkout/[id]/page.tsx
+++ apps/web/app/[locale]/product/checkout/[id]/page.tsx
@@ -12,10 +12,11 @@ import { useTranslations } from "next-intl";
 import { httpClient } from "@app/lib/axios";
 import { useQueries } from "@tanstack/react-query";
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
-import { timer } from "rxjs";
+import { map, timer } from "rxjs";
 import { getUnixTime } from "date-fns";
 import { SelectItem } from "@app/components/Select";
 import DaCucireImage from "@app/assets/images/backing/1.png";
@@ -23,6 +24,7 @@ import TermoadesivaImage from "@app/assets/images/backing/2.png";
 import VelcroAImage from "@app/assets/images/backing/3.png";
 import VelcroBImage from "@app/assets/images/backing/4.png";
 import VelcroABImage from "@app/assets/images/backing/5.png";
+import { RootState } from "@app/store";
 
 type Props = {
   params: {
@@ -40,6 +42,15 @@ let toastShowed = false;
 
 export default function CheckoutProductPage({ params: { id } }: Props) {
   // Hooks
+  const products = useSelector(
+    (state: RootState) => state.persistedProducts.products
+  );
+  const productsIdList = products.map((product)=>{
+   
+     return product.id
+  })
+ 
+  console.log("proooooooooooooo",productsIdList);
   const jwt = useJwt();
   const router = useRouter();
   const t = useTranslations("components.fillInformation");
@@ -72,21 +83,21 @@ export default function CheckoutProductPage({ params: { id } }: Props) {
     };
   }, [product]);
 
-  // Memos
-  const perItemPrice = useMemo(() => {
-    if (!product) return "0";
-    return (parseFloat(product.price as string) / product.quantity).toFixed(2);
-  }, [product]);
+  // // Memos
+  // const perItemPrice = useMemo(() => {
+  //   if (!product) return "0";
+  //   return (parseFloat(product.price as string) / product.quantity).toFixed(2);
+  // }, [product]);
 
-  const backingType = useMemo(() => {
-    if (!product) return null;
-    return backingItems.filter(({ id }) => id === product.backingType)[0];
-  }, [product]);
+  // const backingType = useMemo(() => {
+  //   if (!product) return null;
+  //   return backingItems.filter(({ id }) => id === product.backingType)[0];
+  // }, [product]);
 
-  const selectedPatchType = useMemo(() => {
-    if (!patchTypes || !product || !product.patchType) return null;
-    return patchTypes.filter(({ id }) => id === product.patchType)[0];
-  }, [patchTypes, product]);
+  // const selectedPatchType = useMemo(() => {
+  //   if (!patchTypes || !product || !product.patchType) return null;
+  //   return patchTypes.filter(({ id }) => id === product.patchType)[0];
+  // }, [patchTypes, product]);
 
   // Conditions
   if (!product || !patchTypes) return <Loading />;
@@ -107,8 +118,17 @@ export default function CheckoutProductPage({ params: { id } }: Props) {
       <form
         action={`/product/checkout/${id}/payment`}
         method='POST'
-        className='w-full flex-auto p-6 flex flex-col lg:flex-row items-start justify-center gap-6'>
-        <div className='w-full h-full flex bg-black border-primary-1 border-[1px] rounded-lg text-primary-1  flex-col items-center gap-10'>
+        className='w-full flex-auto p-6 flex flex-col  items-start justify-center gap-6'>
+         {products.map((product) => {
+          return (
+            <ProductContaner
+              key={product.id}
+              patchTypes={patchTypes}
+              product={product}></ProductContaner>
+          );
+        })}
+
+        {/* <div className='w-full h-full flex bg-black border-primary-1 border-[1px] rounded-lg text-primary-1  flex-col items-center gap-10'>
           <div className='w-full  flex items-center justify-start px-6 py-5 border-b-primary-1 border-b-[1px]'>
             <p className='font-bold flex flex-wrap break-all  text-2xl'>
               Product #{product.id}
@@ -212,14 +232,15 @@ export default function CheckoutProductPage({ params: { id } }: Props) {
               <ShoppingItem label='Note' value={`${product.note}`} />
             </div>
           </div>
-        </div>
+        </div> */}
+
         <div className='w-full min-w-[220px] lg:w-max h-max bg-black border-[1px] border-primary-1 rounded-lg py-6 px-4 flex flex-col items-center justify-center gap-10'>
           <div className='w-full h-max flex flex-col items-center justify-center gap-4'>
             <ShoppingItem
               label={`${product.quantity} Items`}
               value={"€" + product.price.toString()}
             />
-            <ShoppingItem label={`Per item`} value={"€" + perItemPrice} />
+            {/* <ShoppingItem label={`Per item`} value={"€" + perItemPrice} /> */}
           </div>
           <div className='w-full h-0.5 bg-primary-1' />
           <Button className='bg-primary-1 mx-auto' style={{ color: "black" }}>
@@ -231,6 +252,128 @@ export default function CheckoutProductPage({ params: { id } }: Props) {
     </div>
   );
 }
+
+const ProductContaner = ({ product, patchTypes }) => {
+  // Memos
+  const perItemPrice = useMemo(() => {
+    if (!product) return "0";
+    return (parseFloat(product.price as string) / product.quantity).toFixed(2);
+  }, [product]);
+
+  const backingType = useMemo(() => {
+    if (!product) return null;
+    return backingItems.filter(({ id }) => id === product.backingType)[0];
+  }, [product]);
+
+  const selectedPatchType = useMemo(() => {
+    if (!patchTypes || !product || !product.patchType) return null;
+    return patchTypes.filter(({ id }) => id === product.patchType)[0];
+  }, [patchTypes, product]);
+
+  // if (!product.isReadyForPayment) {
+  //   return <p></p>;
+  // }
+  return (
+    <div className='w-full h-full flex bg-black border-primary-1 border-[1px] rounded-lg text-primary-1  flex-col items-center gap-10'>
+      <div className='w-full  flex items-center justify-start px-6 py-5 border-b-primary-1 border-b-[1px]'>
+        <p className='font-bold flex flex-wrap break-all  text-2xl'>
+          Product #{product.id}
+        </p>
+      </div>
+      <div className='w-full  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-6 py-5 lg:py-10 gap-20'>
+        <div className='w-full h-max flex flex-col items-start justify-center gap-10 font-bold'>
+          <span>
+            Patch with
+            {product.type
+              ? " " + product.type[0]?.toUpperCase() + product.type?.slice(1)
+              : ""}
+          </span>
+          <div className='w-full flex flex-row items-center justify-between gap-4'>
+            <span>Backing Type: </span>
+            <div className='bg-primary-1 w-max p-2 rounded-lg'>
+              <Image
+                src={backingType?.image || ""}
+                width={52}
+                height={52}
+                alt='Backing Type Image'
+              />
+            </div>
+          </div>
+          {selectedPatchType && selectedPatchType.image ? (
+            <div className='w-full flex flex-row items-center justify-between gap-4'>
+              <span>Patch Type: </span>
+              <div className='bg-primary-1 w-max p-2 rounded-lg'>
+                <Image
+                  src={selectedPatchType?.image || ""}
+                  width={52}
+                  height={52}
+                  alt='Patch Type Image'
+                />
+              </div>
+            </div>
+          ) : null}
+        </div>
+        {product.type === "image" ? (
+          <div className='flex flex-col items-start justify-start gap-4'>
+            <span className='font-bold'>Selected Image</span>
+            <Image
+              src={process.env.NEXT_PUBLIC_BASE_URL + (product.image as any)}
+              width={240}
+              height={240}
+              alt='Product Selected Image'
+              className='rounded-lg border-primary-1 border-[1px]'
+            />
+          </div>
+        ) : (
+          <div className='w-full h-max flex flex-col items-start justify-center gap-10'>
+            <ShoppingItem
+              label='Text'
+              value={product.text ?? "No text provided"}
+            />
+            <ShoppingItem
+              label='Border Color'
+              isColor
+              value={product.borderColor}
+            />
+            <ShoppingItem
+              label='Background Color'
+              isColor
+              value={product.backgroundColor}
+            />
+            <ShoppingItem
+              label='Text Color'
+              isColor
+              value={product.textColor}
+            />
+            <div
+              style={{
+                fontFamily: product.font ? "CustomFont" : undefined,
+              }}
+              className='w-full flex flex-row data-[color=true]:items-start items-center gap-6 justify-between text-primary-1'>
+              <span
+                style={{
+                  color: product.textColor,
+                  borderColor: product.borderColor,
+                  backgroundColor: product.backgroundColor,
+                }}
+                className='font-bold border-4'>
+                {" "}
+                {product.text}
+              </span>
+            </div>
+          </div>
+        )}
+        <div className='w-full h-max flex flex-col items-start justify-center gap-10'>
+          <ShoppingItem label='Width' value={`${product.patchWidth} cm`} />
+          <ShoppingItem label='Height' value={`${product.patchHeight} cm`} />
+        </div>
+        <div className='  w-full'>
+          <ShoppingItem label='Note' value={`${product.note}`} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 type ShoppingItemProps = {
   label?: string;
--- apps/web/app/[locale]/product/editor/[id]/page.tsx
+++ apps/web/app/[locale]/product/editor/[id]/page.tsx
@@ -1,54 +1,56 @@
+/** @format */
+
 "use client";
 
-import {getFonts} from "@app/actions/font";
-import {getPatchTypes} from "@app/actions/patch-type";
-import {getProduct} from "@app/actions/product";
+import { getFonts } from "@app/actions/font";
+import { getPatchTypes } from "@app/actions/patch-type";
+import { getProduct } from "@app/actions/product";
 import ProductEditor from "@app/components/CreateProduct/ProductEditor";
 import Loading from "@app/components/Loading";
 import useJwt from "@app/hooks/useJwt";
-import {useQueries} from "@tanstack/react-query";
-import {redirect} from "next/navigation";
+import { useQueries } from "@tanstack/react-query";
+import { redirect } from "next/navigation";
 
 export default function ProductEidtorPage({
-                                              params,
-                                          }: {
-    params: { id: string };
+  params,
+}: {
+  params: { id: string };
 }) {
-    // Hooks
-    const jwt = useJwt();
+  // Hooks
+  const jwt = useJwt();
 
-    // Queries
-    const [{data: product}, {data: patchTypes}, {data: fonts}] = useQueries(
+  // Queries
+  const [{ data: product }, { data: patchTypes }, { data: fonts }] = useQueries(
+    {
+      queries: [
         {
-            queries: [
-                {
-                    queryKey: ["product", params.id],
-                    queryFn: () => getProduct(params.id, jwt as string),
-                },
-                {
-                    queryKey: ["patch_types", params.id],
-                    queryFn: () => getPatchTypes(),
-                },
-                {
-                    queryKey: ["fonts", params.id],
-                    queryFn: () => getFonts(),
-                },
-            ],
-        }
-    );
+          queryKey: ["product", params.id],
+          queryFn: () => getProduct(params.id, jwt as string),
+        },
+        {
+          queryKey: ["patch_types", params.id],
+          queryFn: () => getPatchTypes(),
+        },
+        {
+          queryKey: ["fonts", params.id],
+          queryFn: () => getFonts(),
+        },
+      ],
+    }
+  );
 
-    // Conditions
-    if (!product || !patchTypes || !fonts) return <Loading/>;
+  // Conditions
+  if (!product || !patchTypes || !fonts) return <Loading />;
 
-    if (product && product.status !== "created")
-        return redirect("/product/create");
-    return (
-        <div className="w-full h-max py-10 flex-auto flex flex-col items-center justify-center">
-            <ProductEditor
-                initialProduct={product}
-                patchTypes={patchTypes}
-                fonts={fonts}
-            />
-        </div>
-    );
+  if (product && product.status !== "created")
+    return redirect("/product/create");
+  return (
+    <div className='w-full h-max py-10 flex-auto flex flex-col items-center justify-center'>
+      <ProductEditor
+        initialProduct={product}
+        patchTypes={patchTypes}
+        fonts={fonts}
+      />
+    </div>
+  );
 }
